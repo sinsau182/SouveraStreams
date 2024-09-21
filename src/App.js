b4edfb0102c5b26d2004ffcdd911671b82674314
sinsau182 <singhsaurav182001@gmail.com>
2024-09-21 15:26:42 +0530

    logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import styled, { ThemeProvider } from "styled-components";
 import bg from './img/bg.png';
 import Navbar from "./Components/Navbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Menu from "./Components/Menu";
 import { darkTheme, lightTheme } from "./utils/Theme";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -38,6 +38,13 @@ const Wrapper = styled.div`
 function App() {
   const [theme, setTheme] = useState(true);
   const [isMenuVisible, setMenuVisible] = useState(window.innerWidth > 768); // Default to true on desktop
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = bg;
+    img.onload = () => setLoading(true);
+  }, []);
 
   const toggleMenu = () => {
     if (window.innerWidth <= 768) {
@@ -50,6 +57,8 @@ function App() {
   });
 
   return (
+    <>
+    {!isLoading && <Loader><div className="spinner"></div></Loader>}
     <AppStyled bg={bg} className="App">
       <ThemeProvider theme={theme ? darkTheme : lightTheme}>
         <Navbar onMenuButtonClick={toggleMenu} MenuCheck={isMenuVisible}/>
@@ -74,6 +83,7 @@ function App() {
         </Container>
       </ThemeProvider>
     </AppStyled>
+    </>
   );
 }
 
@@ -81,6 +91,63 @@ const AppStyled = styled.div`
   width: 100%;
   background-image: url(${props => props.bg});
   position: relative;
+`;
+
+const Loader = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+  background: linear-gradient(135deg, #f0f4f8 0%, #d9e2ec 100%);
+  color: #2e3a59;
+  font-size: 1.6rem;
+  font-weight: 600;
+  letter-spacing: 0.05em;
+  text-transform: uppercase;
+  position: relative;
+  padding: 0 20px;
+
+  .spinner {
+    border: 6px solid rgba(0, 0, 0, 0.1);
+    border-left-color: #2e3a59;
+    border-radius: 50%;
+    width: 70px;
+    height: 70px;
+    animation: spin 1.2s linear infinite;
+    margin-bottom: 2rem;
+  }
+
+  &::after {
+    content: 'Loading...';
+    color: #2e3a59;
+    margin-top: 1rem;
+  }
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    font-size: 1.3rem;
+    .spinner {
+      width: 60px;
+      height: 60px;
+    }
+  }
+
+  @media screen and (max-width: 480px) {
+    font-size: 1.1rem;
+    .spinner {
+      width: 50px;
+      height: 50px;
+    }
+  }
 `;
 
 export default App;
